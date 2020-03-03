1a8b06a1c84d1765ba7d903a6fcb3866c19dd282
= <andre@bwcenter.com.br>
2020-03-03 16:47:34 -0300

    Gerando erro ao não implementar método _template nas Views

--- client/js/app/views/View.js
+++ client/js/app/views/View.js
@@ -4,6 +4,10 @@ class View {
         this._elemento = elemento;
     }
 
+    _template() {
+        throw new Error('O método template deve ser implementado');
+    }
+
     update(model) {
         this._elemento.innerHTML = this._template(model);
     }
