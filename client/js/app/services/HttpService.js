4fdc3214f8d13d07ed74349c102b768661d574bf
André Bonfante <andre458bonfante@gmail.com>
2020-03-26 17:38:35 -0300

    POST no HttpService e formulario de cadastro

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -5,7 +5,6 @@ class HttpService {
 
             let xhr = new XMLHttpRequest();
             xhr.open('GET', url);
-    
             xhr.onreadystatechange = () => {
 
                 if (xhr.readyState === 4) {
@@ -15,9 +14,27 @@ class HttpService {
                         reject(xhr.responseText);
                 }
             };
-    
             xhr.send();
+        });
+    }
 
+    post(url, dados) {
+        return new Promise((resolve, reject) => {
+
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onreadystatechange = () => {
+
+                if (xhr.readyState === 4) {
+
+                    xhr.status === 200 ?
+                        resolve(JSON.parse(xhr.responseText)) :
+                        reject(xhr.responseText);
+                }
+
+            };
+            xhr.send(JSON.stringify(dados));
         });
     }
 
