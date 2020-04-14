8836815e0aa2b29282367c500a408ee7eaae211e
= <andre@bwcenter.com.br>
2020-04-14 09:39:52 -0300

    Implementando factory de conexões com Factory e Monkey Patch

---
+++ client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,54 @@
+var ConnectionFactory = (function() {
+
+    const stores = ['negociacoes'];
+    const version = 4;
+    const dbName = 'aluraframe';
+
+    var connection = null;
+    var closeConnection = null;
+
+    return class ConnectionFactory {
+
+        constructor() {
+            throw new Error('Não é possível criar instâncias de ConnectionFactory')
+        }
+
+        static getConnection() {
+            return new Promise((resolve, reject) => {
+
+                let openRequest = window.indexedDB.open(dbName, version);
+                openRequest.onupgradeneeded = e => ConnectionFactory._createStores(e.target.result);
+                openRequest.onsuccess = e => {
+                    
+                    if (!connection) {
+                        connection = e.target.result;
+                        closeConnection = connection.close.bind(connection);
+                        connection.close = () => {
+                            throw new Error('Não é possível fechar a conexão diretamente');
+                        }
+                    }
+                    
+                    resolve(connection);
+                };
+                openRequest.onerror = e => reject(e.target.error.name);
+            });
+        }
+
+        static closeConnection() {
+            if (connection) {
+                closeConnection();
+                connection = null;
+            }
+        }
+
+        static _createStores(connection) {
+            stores.forEach(store => {
+                if (connection.objectStoreNames.contains(store)) {
+                    connection.deleteObjectStore(store);
+                }
+
+                connection.createObjectStore(store, { autoincrement: true });
+            });
+        }
+    }
+})();
